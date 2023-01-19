ddd69426f63bdfafa5fdc4a878c348db74ad9701
Lekan <jimoridwanlekan@gmail.com>
2023-01-19 12:01:23 +0100

    Modify Pet and App to use JSX

--- src/App.jsx
+++ src/App.jsx
@@ -5,24 +5,10 @@ import Pet from "./Pet";
 
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Havanese",
-    }),
-    React.createElement(Pet, {
-      name: "Pepper",
-      animal: "Bird",
-      breed: "Cockatiel",
-    }),
-    React.createElement(Pet, {
-      name: "Doink",
-      animal: "Cat",
-      breed: "Mixed",
-    }),
-  ]);
+  <div>
+    <h1>Adopt Me!</h1>
+    <Pet name="Luna" animal="Dog" breed="Havanese" />
+  </div>
 };
 
 const container = document.getElementById("root");
