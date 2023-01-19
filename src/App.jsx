25f1c38fc48b3d583cd25732587efe14f3668144
Lekan <jimoridwanlekan@gmail.com>
2023-01-19 19:23:44 +0100

    Format eslint rules

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,11 @@ const App = () => {
   <div>
     <h1>Adopt Me!</h1>
     <Pet name="Luna" animal="Dog" breed="Havanese" />
+    <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
+    <Pet name="Doink" animal="Cat" breed="Mixed" />
   </div>
 };
 
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
-root.render(React.createElement(App));
+root.render(<App />);
