6427bddc0d082a051e19632ac5e56385a69efda2
Lekan <jimoridwanlekan@gmail.com>
2023-01-20 10:44:58 +0100

    Add searchParams file

---
+++ src/SearchParams.jsx
@@ -0,0 +1,77 @@
+import { useState } from "react";
+const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
+
+const SearchParams = () => {
+  const [location, setLocation] = useState("");
+  const [animal, setAnimal] = useState("");
+  const [breed, setBreed] = useState("");
+  const breeds = [];
+  // const [animal, setAnimal] = useState("");
+  // const [breed, setBreed] = useState("");
+  // const breeds = [];
+  return (
+    <div className="search-params">
+      <form>
+        <label htmlFor="location">
+          Location
+          <input
+            id="location"
+            value={location}
+            placeholder="Location"
+            onChange={(e) => setLocation(e.target.value)}
+          />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(e) => {
+              setAnimal(e.target.value);
+              setBreed("");
+            }}
+            onBlur={(e) => {
+              setAnimal(e.target.value);
+              setBreed("");
+            }}
+          >
+            <option />
+            {ANIMALS.map((animal) => (
+              <option key={animal} value={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="breed">
+          Breed
+          <select
+            id="breed"
+            disabled={breeds.length === 0}
+            value={breed}
+            onChange={(e) => {
+              setBreed(e.target.value);
+              // updateBreed("");
+            }}
+            // onBlur={(e) => {
+            //   setAnimal(e.target.value);
+            //   updateBreed("");
+            // }}
+          >
+            <option />
+            {breeds.map((breed) => (
+              <option key={breed} value={breed}>
+                {breed}
+              </option>
+            ))}
+          </select>
+        </label>
+        ;{/* <AnimalDropdown /> */}
+        {/* <BreedDropdown /> */}
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default SearchParams;
