d263eae8e03351f2250ef61ba476a2028f96f49b
Lekan <jimoridwanlekan@gmail.com>
2023-02-02 18:54:20 +0100

    Add usebreed custom hook

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import Pet from "./Pet";
+import useBreedList from "./useBreedList";
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 
 const SearchParams = () => {
@@ -7,6 +8,7 @@ const SearchParams = () => {
   const [animal, setAnimal] = useState("");
   const [breed, setBreed] = useState("");
   const [pets, setPets] = useState([]);
+  const [breeds] = useBreedList(animal);
 
   useEffect(() => {
     requestPets(); 
@@ -19,9 +21,6 @@ const SearchParams = () => {
     const json = await res.json();
     setPets(json.pets);
   }
-
-
-  const breeds = [];
   
   return (
     <div className="search-params">
