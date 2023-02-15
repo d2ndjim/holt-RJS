cd6dcad45a9581647b1ff26a527909e51994dd92
Lekan <jimoridwanlekan@gmail.com>
2023-02-15 18:52:02 +0100

    Add react query in place of useEffect

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,17 +1,32 @@
 import { useParams } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import fetchPet from "./FetchPet";
+
 const Details = () => {
   const { id } = useParams();
-  // const { animal, breed, city, state, description, name } = props;
+  const results = useQuery(["details", id], fetchPet);
+
+  if (results.isLoading) {
+    return (
+      <div className="loading-pane">
+        <h2 className="loader">⭕</h2>
+
+      </div>
+    )
+  }
+
+  const pet = results.data.pets[0];
+
   return (
     <div className="details">
-      <h1>{id}</h1>
-      {/* <Carousel media={props.media} />
       <div>
-        <h1>{name}</h1>
-        <h2>{`${animal} - ${breed} - ${city}, ${state}`}</h2>
-        <button>Adopt {name}</button>
-        <p>{description}</p>
-      </div> */}
+        <h1>{pet.name}</h1>
+        <h2>
+          {pet.animal} - {pet.breed} - {pet.city}, {pet.state}
+          <button>Adopt {pet.name}</button>
+          <p>{pet.description}</p>
+        </h2>
+      </div>
     </div>
   );
 }
