a36bf54c237f3bc315c40c961a2112835c5dcbdc
Lekan <jimoridwanlekan@gmail.com>
2023-02-15 17:29:42 +0100

    Add the refactored component to search params

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import Pet from "./Pet";
+import Results from "./Results";
 import useBreedList from "./useBreedList";
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 
@@ -82,9 +83,7 @@ const SearchParams = () => {
         {/* <BreedDropdown /> */}
         <button>Submit</button>
       </form>
-      {pets.map((pet) => (
-        <Pet key={pet.id} name={pet.name} animal={pet.animal} breed={pet.breed} />
-      ))}
+      <Results pets={pets} />
     </div>
   );
 }
