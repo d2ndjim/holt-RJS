b069df0db839ee4fdd19458d9ae107bbc5974f5d
Lekan <jimoridwanlekan@gmail.com>
2023-02-27 15:47:27 +0100

    Add ClientApp

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState, lazy, Suspense } from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import AdoptedContext from "./AdoptedPets";
 import Spinner from "./Spinner";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -26,30 +25,23 @@ const App = () => {
         background: "url(http://pets-images.dev-apis.com/pets/wallpaperA.jpg)",
       }}
     >
-      <BrowserRouter>
-        <QueryClientProvider client={queryClient}>
-          <AdoptedContext.Provider value={adoptedPets}>
-            <Suspense fallback={<Spinner />}>
-              <header className="mb-10 w-full bg-gradient-to-b from-yellow-400 via-orange-500 to-red-500 p-7 text-center">
-                <Link
-                  className="text-6xl text-white hover:text-gray-200"
-                  to="/"
-                >
-                  Adopt Me!
-                </Link>
-              </header>
-              <Routes>
-                <Route path="/details/:id" element={<Details />} />
-                <Route path="/" element={<SearchParams />} />
-              </Routes>
-            </Suspense>
-          </AdoptedContext.Provider>
-        </QueryClientProvider>
-      </BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <AdoptedContext.Provider value={adoptedPets}>
+          <Suspense fallback={<Spinner />}>
+            <header className="mb-10 w-full bg-gradient-to-b from-yellow-400 via-orange-500 to-red-500 p-7 text-center">
+              <Link className="text-6xl text-white hover:text-gray-200" to="/">
+                Adopt Me!
+              </Link>
+            </header>
+            <Routes>
+              <Route path="/details/:id" element={<Details />} />
+              <Route path="/" element={<SearchParams />} />
+            </Routes>
+          </Suspense>
+        </AdoptedContext.Provider>
+      </QueryClientProvider>
     </div>
   );
 };
 
-const container = document.getElementById("root");
-const root = createRoot(container);
-root.render(<App />);
+export default App;
